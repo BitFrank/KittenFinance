4f3f138218481a4dbfc0136a8ee51bf6bf3e1640
BitFrank <36596828+BitFrank@users.noreply.github.com>
2021-04-06 22:53:04 +0800

    Add files via upload

---
+++ docs/new/i18n.js
@@ -0,0 +1,100 @@
+"use strict";
+
+let I18N = {
+    "loading": {
+        en: `Loading...`,
+        zh: `正在载入...`,
+    },
+    "connecting_metamask": {
+        en: `Connecting Metamask (please unlock)`,
+        zh: `正在连接 Metamask (请解锁)`,
+    },
+    "welcome_kf": {
+        en: `Welcome to KittenFinance NextGen Preview.
+    <br><br>This will be a unified UI for you to manage all assets, and to use all 
+    KittenFinance functions: swap, lending, option, liquid, dex, farming and more.
+    <br><br>Click the titlebar of a panel to shrink / restore.<br>Drag titlebar to move (if you are using a computer).`,
+        zh: `欢迎来到 KittenFinance 新版本的预览。
+    <br><br>这里将会是个统一的界面，让你可以管理所有资产，和使用所有
+    KittenFinance 功能：交易，借贷，期权，liquid，dex，挖矿，等等。
+    <br><br>点击面板的标题栏，可以缩小 / 还原。<br>拖动标题栏，可以移动（如果你使用电脑）。`
+    },
+    'asset': {
+        zh: `资产`,
+        jp: `資産`,
+        kr: `자산`,
+    },
+    'assets': {
+        zh: `资产`,
+        jp: `資産`,
+        kr: `자산`
+    },
+    'amount': {
+        zh: `数额`,
+        jp: `量`,
+        kr: `금액`
+    },
+    'amt': {
+        zh: `数额`,
+        jp: `量`,
+        kr: `금액`
+    },
+    'price': {
+        zh: `价格`,
+        jp: `価格`,
+        kr: `가격`
+    },
+    'click_trade': {
+        en: 'Click an asset to trade.',
+        zh: `点击资产，打开交易界面。`
+    },
+    'account': {
+        zh: `账户`,
+        jp: `アカウント`,
+        kr: `계정`,
+        es: `cuenta`,
+        ru: `аккаунт`,
+        de: `Konto`,
+        fr: `compte`,        
+    },
+    'lending': {
+        zh: `借贷`
+    },
+    'token': {
+        zh: `代币`
+    },
+    'config': {
+        zh: `设置`,
+        jp: `設定`,
+        kr: `설정`,
+        es: `config`,
+        ru: `конфигурация`,
+        de: `Konfiguration`,
+        fr: `config`,
+    },
+    'block': { // block xxx
+        zh: `区块`,
+        jp: `ブロック`,
+        kr: `블록`
+    },
+    'updated': {
+        zh: '更新于',
+    },
+    's ago': { // updated xx s ago
+        zh: '秒 前',
+    },
+    'reset layout': {
+        zh: `重置界面`,
+        jp: `リセットインターフェース`,
+        kr: `레이아웃 재설정`,
+        es: `restablecer el diseño`,
+        ru: `схема сброса`,
+        de: `Layout zurücksetzen`,
+        fr: `réinitialiser la mise en page`,        
+    },
+    'visit old': { // visit old Kitten.Finance
+        zh: `访问旧版`,
+        jp: `旧ウェブサイトを見る`,
+        kr: `이전 웹 사이트 방문`
+    }
+}
