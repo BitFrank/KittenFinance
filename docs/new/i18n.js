eccae1ad9140cd4210bde89f8d9fa4c4bec94d44
BitFrank <36596828+BitFrank@users.noreply.github.com>
2021-04-08 19:51:48 +0800

    Update i18n.js

--- docs/new/i18n.js
+++ docs/new/i18n.js
@@ -9,16 +9,27 @@ let I18N = {
         en: `Connecting Metamask (please unlock)`,
         zh: `正在连接 Metamask (请解锁)`,
     },
+    //============== welcome panel ==============
     "welcome_kf": {
         en: `Welcome to KittenFinance NextGen Preview.
-    <br><br>This will be a unified UI for you to manage all assets, and to use all 
-    KittenFinance functions: swap, lending, option, liquid, dex, farming and more.
+    <br><br>This is a unified UI for you to manage all assets, and to use all 
+    KittenFinance functions: swap, lending, option, vaults and more.
     <br><br>Click the titlebar of a panel to shrink / restore.<br>Drag titlebar to move (if you are using a computer).`,
         zh: `欢迎来到 KittenFinance 新版本的预览。
-    <br><br>这里将会是个统一的界面，让你可以管理所有资产，和使用所有
-    KittenFinance 功能：交易，借贷，期权，liquid，dex，挖矿，等等。
+    <br><br>这里是统一的界面，让你可以管理所有资产，使用所有
+    KittenFinance 功能：交易，借贷，期权，挖矿，等等。
     <br><br>点击面板的标题栏，可以缩小 / 还原。<br>拖动标题栏，可以移动（如果你使用电脑）。`
     },
+    'account': {
+        zh: `账户`,
+        jp: `アカウント`,
+        kr: `계정`,
+        es: `cuenta`,
+        ru: `аккаунт`,
+        de: `Konto`,
+        fr: `compte`,        
+    },    
+    //============== asset panel ==============
     'asset': {
         zh: `资产`,
         jp: `資産`,
@@ -48,21 +59,29 @@ let I18N = {
         en: 'Click an asset to trade.',
         zh: `点击资产，打开交易界面。`
     },
-    'account': {
-        zh: `账户`,
-        jp: `アカウント`,
-        kr: `계정`,
-        es: `cuenta`,
-        ru: `аккаунт`,
-        de: `Konto`,
-        fr: `compte`,        
-    },
+    //============== lending panel ==============
     'lending': {
         zh: `借贷`
     },
     'token': {
         zh: `代币`
     },
+    'borrow price': {
+        zh: `借贷价格`
+    },
+    'apy%': {
+        zh: `年化利息%`
+    },
+    'expiry days': {
+        zh: `借贷期限`
+    },
+    'vault asset': {
+        zh: `vault 资产`
+    },
+    'your asset': {
+        zh: `你的资产`
+    },
+    //============== config panel ==============
     'config': {
         zh: `设置`,
         jp: `設定`,
